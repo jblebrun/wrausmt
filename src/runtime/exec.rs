d62122ccb91645f098adf8a2ac214b94c864ae73
Jason LeBrun <jblebrun@gmail.com>
2021-04-02 09:11:09 -0700

    Add missing exec file

---
+++ src/runtime/exec.rs
@@ -0,0 +1,34 @@
+use super::super::instructions::Inst;
+use super::super::instructions::Inst::*;
+use super::Runtime;
+use super::stack::StackEntry::*;
+
+/// Implementation of instruction implementation for this runtime.
+impl Inst {
+    pub fn execute(&self, runtime: &mut Runtime) {
+        match self {
+            // Get the local variable for the specified index,
+            // and place it on the stack.
+            LocalGet(idx) => {
+                let val = match &runtime.current_frame {
+                    Some(frame) => frame.locals[*idx as usize],
+                    _ => panic!("no current frame")
+                };
+                runtime.stack.push(Value(val));
+            },
+
+            // Place the provided constant value on the stack.
+            Const32(val) => {
+                runtime.stack.push(Value(*val as u64));
+            },
+
+            // Pop the top two values from the stack.
+            // Push the result of adding those two values.
+            Add32 => {
+                let a = runtime.stack.pop_value();
+                let b = runtime.stack.pop_value();
+                runtime.stack.push(Value(a+b));
+            }
+        }
+    }
+}
