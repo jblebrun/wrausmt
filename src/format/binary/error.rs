e79f2b7e352cea0c1d1a8eb93831a0cd2a0367be
Jason LeBrun <jblebrun@gmail.com>
2021-04-03 11:01:39 -0700

    Error handling work

---
+++ src/format/binary/error.rs
@@ -0,0 +1,68 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum ParseError {
+    Error(String),
+    Wrapped(String, Box<dyn Error>)
+}
+
+pub type Result<T> = std::result::Result<T, ParseError>;
+
+impl ParseError {
+    pub fn new(msg: String) -> ParseError {
+        ParseError::Error(msg)
+    }
+}
+
+impl From<&str> for ParseError {
+    fn from(msg: &str) -> ParseError {
+        ParseError::new(msg.to_string())
+    }
+}
+
+pub trait ParseErrorFrom {
+    fn wrap(self, msg: &str) -> ParseError;
+}
+
+pub trait ResultFrom<T, E> {
+    fn wrap(self, msg: &str) -> Result<T>;
+}
+
+impl ParseErrorFrom for std::io::Error {
+    fn wrap(self, msg: &str) ->  ParseError {
+        ParseError::Wrapped(msg.to_string(), Box::new(self))
+    }
+}
+
+impl ParseErrorFrom for std::string::FromUtf8Error {
+    fn wrap(self, msg: &str) ->  ParseError {
+        ParseError::Wrapped(msg.to_string(), Box::new(self))
+    }
+}
+
+impl ParseErrorFrom for ParseError {
+    fn wrap(self, msg: &str) ->  ParseError {
+        ParseError::Wrapped(msg.to_string(), Box::new(self))
+    }
+}
+
+impl <T, E : ParseErrorFrom> ResultFrom<T, E>  for std::result::Result<T,E> {
+    fn wrap(self, msg: &str)-> Result<T> {
+        self.map_err(|e| e.wrap(msg))
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt<'l>(&'l self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            ParseError::Error(msg) => write!(f, "  {}", msg),
+            ParseError::Wrapped(msg, src) => write!(f, "  {}\n{}", msg, src)
+        }
+    }
+}
+
+impl Error for ParseError { }
+
+
+
--- src/format/binary/mod.rs
+++ src/format/binary/mod.rs
@@ -1,36 +1,36 @@
-use std::io::Bytes;
+mod error;
 use std::io::Read;
-use std::io::BufReader;
-use std::fs::File;
 use std::io;
-use std::io::prelude::*;
+use std::fs::File;
 use super::super::module::*;
 use super::super::types::*;
 use super::super::types::ValueType::*;
 use super::super::types::NumType::*;
 use super::super::types::RefType::*;
+use error::*;
+
 
 trait WasmParser: Read {
-    fn skip_section(&mut self, len: u32) -> io::Result<()> {
+    fn skip_section(&mut self, len: u32) -> Result<()> {
         let mut section: Vec<u8> = vec![0; len as usize];
-        self.read_exact(&mut section)?;
+        self.read_exact(&mut section).wrap("reading skipped content")?;
         Ok(())
     }
 
-    fn parse_section_0(&mut self, len: u32) -> io::Result<()> {
+    fn parse_section_0(&mut self, len: u32) -> Result<()> {
         let mut section: Vec<u8> = vec![0; len as usize];
-        self.read_exact(&mut section)?;
+        self.read_exact(&mut section).wrap("reading custom content")?;
         println!("CUSTOM: {:?}", section);
         Ok(())
     }
     
-    fn next_leb_128(&mut self) -> io::Result<u32> {
+    fn next_leb_128(&mut self) -> Result<u32> {
         let mut result: u32 = 0;
         let mut pos = 0;
         let mut completed = false;
 
         for next in self.bytes() {
-            let x = next?;
+            let x = next.wrap("reading leb 128 byte")?;
             result |= ((x & 0x7f) as u32) << pos;
             if x & 0x80 == 0x00 { 
                 completed = true;
@@ -38,83 +38,81 @@ trait WasmParser: Read {
             }
             pos += 7;
             if pos > 31 {
-                return Err(io::Error::new(io::ErrorKind::InvalidData, "u32 LEB128 data is too long"));
+                return Err(ParseError::new(format!("u32 LEB128 data is too long")));
             }
         }
 
         if !completed {
-            return Err(io::Error::new(io::ErrorKind::UnexpectedEof, "Reached end of input before parsing LEB128"));
+            return Err(ParseError::new(format!("Reached end of input before parsing LEB128")));
         }
         
         return Ok(result);
     }
 
-    fn parse_name(&mut self) -> io::Result<String> {
-        let length = self.next_leb_128()?;
+    fn parse_name(&mut self) -> Result<String> {
+        let length = self.next_leb_128().wrap("parsing length")?;
         
         let mut bs: Vec<u8> = vec![0; length as usize];
 
-        self.read_exact(&mut bs)?;
+        self.read_exact(&mut bs).wrap("reading name data")?;
 
-        match String::from_utf8(bs) {
-            Ok(item) => Ok(item),
-            Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, "Utf8 error"))
-        }
+        String::from_utf8(bs).wrap("parsing name data")
     }
     
-    fn parse_result_type(&mut self) -> io::Result<Box<ResultType>> {
-        let item_count = self.next_leb_128()?;
+    fn parse_result_type(&mut self) -> Result<Box<ResultType>> {
+        let item_count = self.next_leb_128().wrap("parsing count")?;
 
         (0..item_count).map(|_| {
             self.parse_value_type()
         }).collect()
     }
 
-    fn expect_byte(&mut self, expect: u8) -> io::Result<u8> {
-        let actual = self.parse_next_byte()?;
-        match actual {
-            expect => Ok(actual),
-            _ => Err(io::Error::new(io::ErrorKind::InvalidData, format!("Expected {} but got {}", expect, actual)))
+    fn expect_byte(&mut self, expect: u8) -> Result<u8> {
+        let actual = self.parse_next_byte().wrap("parsing byte")?;
+        if actual != expect {
+            Err(ParseError::new(format!("Expected {} but got {}", expect, actual)))
+        } else {
+            Ok(actual)
         }
     }
 
-    fn parse_next_byte(&mut self) -> io::Result<u8> {
+    fn parse_next_byte(&mut self) -> Result<u8> {
         match self.bytes().next() {
             Some(Ok(b)) => Ok(b),
             Some(Err(e)) => Err(e),
             _ => Err(io::Error::new(io::ErrorKind::UnexpectedEof, ""))
-        }
+        }.wrap("parsing next byte")
     }
 
-    fn parse_section_1(&mut self, len: u32) -> io::Result<Box<[FunctionType]>> { 
+    fn parse_section_1(&mut self, len: u32) -> Result<Box<[FunctionType]>> { 
         let result: Vec<FunctionType>;
-        let items = self.next_leb_128()?;
+        let items = self.next_leb_128().wrap("parsing item count")?;
 
         (0..items).map(|_| {
-            self.expect_byte(0x60)?;
+            self.expect_byte(0x60).wrap("checking type byte")?;
             Ok(FunctionType {
-                params: self.parse_result_type()?,
-                result: self.parse_result_type()?
+                params: self.parse_result_type().wrap("parsing params")?,
+                result: self.parse_result_type().wrap("parsing result")?
             })
         }).collect()
     }
     
     /// Section 2: Imports
-    fn parse_section_2(&mut self) -> io::Result<Box<[Import]>> {
-        let items = self.next_leb_128()?;
+    fn parse_section_2(&mut self) -> Result<Box<[Import]>> {
+        let items = self.next_leb_128().wrap("parsing count")?;
 
         (0..items).map(|_| {
             Ok(Import {
-                module_name: self.parse_name()?,
-                name: self.parse_name()?, 
+                module_name: self.parse_name().wrap("parsing module name")?,
+                name: self.parse_name().wrap("parsing name")?, 
                 desc: {
-                    let kind = self.parse_next_byte()?;
+                    let kind = self.parse_next_byte().wrap("parsing kind")?;
                     match kind {
-                        0 => ImportDesc::Func(self.next_leb_128()?),
-                        1 => ImportDesc::Table(self.parse_table_type()?),
-                        2 => ImportDesc::Memory(self.parse_memory_type()?),
-                        3 => ImportDesc::Global(self.parse_global_type()?),
-                        _ => return invalid_data(format!("unknown import desc {}", kind))
+                        0 => ImportDesc::Func(self.next_leb_128().wrap("parsing func")?),
+                        1 => ImportDesc::Table(self.parse_table_type().wrap("parsing table")?),
+                        2 => ImportDesc::Memory(self.parse_memory_type().wrap("parsing memory")?),
+                        3 => ImportDesc::Global(self.parse_global_type().wrap("parsing global")?),
+                        _ => return Err(ParseError::new(format!("unknown import desc {}", kind)))
                     }
                 }
             })
@@ -122,77 +120,73 @@ trait WasmParser: Read {
         }).collect()
     }
 
-    fn parse_value_type(&mut self) -> io::Result<ValueType> {
-        convert_value_type(self.parse_next_byte()?)
+    fn parse_value_type(&mut self) -> Result<ValueType> {
+        convert_value_type(self.parse_next_byte().wrap("fetching value type")?)
     }
     
-    fn parse_ref_type(&mut self) -> io::Result<RefType> {
-        convert_ref_type(self.parse_next_byte()?)
+    fn parse_ref_type(&mut self) -> Result<RefType> {
+        convert_ref_type(self.parse_next_byte().wrap("fetching ref type")?)
     }
 
-    fn parse_bool(&mut self) -> io::Result<bool> {
-       let bool_byte = self.parse_next_byte()?;
+    fn parse_bool(&mut self) -> Result<bool> {
+       let bool_byte = self.parse_next_byte().wrap("fetching bool")?;
        match bool_byte {
            0 => Ok(false),
            1 => Ok(true),
-           _ => invalid_data(format!("invalid bool value {}", bool_byte))
+           _ => Err(ParseError::new(format!("invalid bool value {}", bool_byte)))
        }
     }
 
-    fn parse_limits(&mut self) -> io::Result<Limits> {
-        let has_upper = self.parse_bool()?;
+    fn parse_limits(&mut self) -> Result<Limits> {
+        let has_upper = self.parse_bool().wrap("parsing has upper")?;
         Ok(Limits {
-            lower: self.next_leb_128()?,
-            upper: if has_upper { Some(self.next_leb_128()?) } else { None }
+            lower: self.next_leb_128().wrap("parsing lower")?,
+            upper: if has_upper { Some(self.next_leb_128().wrap("parsing upper")?) } else { None }
         })
     }
 
-    fn parse_table_type(&mut self) -> io::Result<TableType> {
+    fn parse_table_type(&mut self) -> Result<TableType> {
         Ok(TableType {
-            reftype: self.parse_ref_type()?,
-            limits: self.parse_limits()?
+            reftype: self.parse_ref_type().wrap("parsing reftype")?,
+            limits: self.parse_limits().wrap("parsing limits")?
         })
     }
 
-    fn parse_global_type(&mut self) -> io::Result<GlobalType> {
+    fn parse_global_type(&mut self) -> Result<GlobalType> {
         Ok(GlobalType {
-            valtype: self.parse_value_type()?,
-            mutable: self.parse_bool()?
+            valtype: self.parse_value_type().wrap("parsing value")?,
+            mutable: self.parse_bool().wrap("parsing mutable")?,
         })
     }
 
-    fn parse_memory_type(&mut self) -> io::Result<MemType> {
+    fn parse_memory_type(&mut self) -> Result<MemType> {
         Ok(MemType {
-            limits: self.parse_limits()?
+            limits: self.parse_limits().wrap("parsing limits")?
         })
     }
 }
 
 impl <I> WasmParser for I where I:Read {}
 
-fn invalid_data<T>(msg: String) -> io::Result<T> {
-    Err(io::Error::new(io::ErrorKind::InvalidData, msg))
-}
-
-pub fn convert_number_type(byte: u8) -> io::Result<NumType> {
+pub fn convert_number_type(byte: u8) -> Result<NumType> {
     match byte {
         0x7F => Ok(I32),
         0x7E => Ok(I64),
         0x7C => Ok(F32),
         0x7C => Ok(F64),
-        _ => invalid_data(format!("{} does not encode a NumType", byte))
+        _ => Err(ParseError::new(format!("{} does not encode a NumType", byte)))
     }
 }
 
-pub fn convert_ref_type(byte: u8) -> io::Result<RefType> {
+pub fn convert_ref_type(byte: u8) -> Result<RefType> {
     match byte {
         0x70 => Ok(Func),
         0x6F => Ok(Extern),
-        _ => Err(io::Error::new(io::ErrorKind::InvalidData, format!("{} does not encode a RefType", byte)))
+        _ => Err(ParseError::new(format!("{} does not encode a RefType", byte)))
     }
 }
 
-pub fn convert_value_type(byte: u8) -> io::Result<ValueType> {
+pub fn convert_value_type(byte: u8) -> Result<ValueType> {
     match byte {
         0x7F => Ok(Num(I32)),
         0x7E => Ok(Num(I64)),
@@ -200,7 +194,7 @@ pub fn convert_value_type(byte: u8) -> io::Result<ValueType> {
         0x7C => Ok(Num(F64)),
         0x70 => Ok(Ref(Func)),
         0x6F => Ok(Ref(Extern)),
-        _ => Err(io::Error::new(io::ErrorKind::InvalidData, format!("{} does not encode a ValueType", byte)))
+        _ => Err(ParseError::new(format!("{} does not encode a ValueType", byte)))
     }
 }
 
@@ -229,7 +223,7 @@ impl <T : Read> Read for CountRead<T> {
     }
 }
 
-pub fn parse<R>(src: &mut R) -> io::Result<Module> where R : Read {
+pub fn parse<R>(src: &mut R) -> Result<Module> where R : Read {
     let reader = &mut CountRead::new(src);
 
     let mut buf: [u8; 4] = [0; 4];
@@ -249,14 +243,14 @@ pub fn parse<R>(src: &mut R) -> io::Result<Module> where R : Read {
         section: u8, 
         module: &mut Module, 
         reader: &mut R,
-    ) -> io::Result<()> {
-        let len = reader.next_leb_128()?;
+    ) -> Result<()> {
+        let len = reader.next_leb_128().wrap("parsing length")?;
         println!("SECTION {} ({:x}) -- LENGTH {}", section, section, len);
         match section {
-            0 => reader.parse_section_0(len)?,
-            1 => module.types = reader.parse_section_1(len)?,
-            2 => module.imports = reader.parse_section_2()?,
-            _ => reader.skip_section(len)?
+            0 => reader.parse_section_0(len).wrap("parsing custom")?,
+            1 => module.types = reader.parse_section_1(len).wrap("parsing types")?,
+            2 => module.imports = reader.parse_section_2().wrap("parsing imports")?,
+            _ => reader.skip_section(len).wrap("skipping section")?
         }
         Ok(())
     }
@@ -264,14 +258,14 @@ pub fn parse<R>(src: &mut R) -> io::Result<Module> where R : Read {
     loop {
         let section = match reader.bytes().next() {
             Some(Ok(v)) => v,
-            Some(Err(e)) => return Err(e),
+            Some(Err(e)) => return Err(e).wrap("parsing section"),
             None => break
         };
 
         match parse_section(section, &mut module, reader) {
             Err(e) => {
                 let consumed = reader.consumed();
-                println!("Error parsing: {:?} at {} (0x{:x})", e, consumed, consumed);
+                println!("\n\nError parsing at {} (0x{:x}), caused by:\n{}\n\n", consumed, consumed, e);
                 break;
             }
             _ => ()
