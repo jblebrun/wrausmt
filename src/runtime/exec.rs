60b2006c74de0d8ea22ead83e2c2fc714317aba3
jblebrun <jblebrun@gmail.com>
2021-04-05 08:20:40 -0700

    Cleaning up exec patterns. (#3)
    Start to standardize the operations performed by an instruction into a
    trait.

--- src/runtime/exec.rs
+++ src/runtime/exec.rs
@@ -4,6 +4,8 @@ use super::super::instructions::*;
 use super::super::error::*;
 use super::stack::Frame;
 use std::rc::Rc;
+use super::super::types::*;
+use std::convert::TryFrom;
 
 struct InvocationContext<'l> {
     runtime: &'l mut Runtime,
@@ -11,12 +13,57 @@ struct InvocationContext<'l> {
     pc: usize
 }
 
-impl<'l> InvocationContext<'l> {
+trait ActivationContext {
+    fn next_byte(&mut self) -> u8;
+    fn next_u32(&mut self) -> Result<u32>;
+    fn get_local(&mut self, idx: u32) -> Result<Value>;
+    fn set_local(&mut self, idx: u32, val: Value) -> Result<()>;
+    fn push_value(&mut self, val: Value) -> Result<()>;
+    fn push<T : Into<Value>>(&mut self, val: T) -> Result<()>;
+    fn pop_value(&mut self) -> Result<Value>;
+    fn pop<T : TryFrom<Value, Error=Error>>(&mut self) -> Result<T>;
+}
+
+impl <'l> ActivationContext for InvocationContext<'l> {
+    fn next_byte(&mut self) -> u8{
+        self.body[self.pc]
+    }
+
     fn next_u32(&mut self) -> Result<u32> {
         let result = u32::from_le_bytes(self.body[self.pc..self.pc+4].try_into().wrap("idx")?);
         self.pc += 4;
         Ok(result)
     }
+
+    fn get_local(&mut self, idx: u32) -> Result<Value> {
+        Ok(self.current_frame()?.locals.borrow()[idx as usize])
+    }
+
+    fn set_local(&mut self, idx: u32, val: Value) -> Result<()> {
+        self.current_frame()?.locals.borrow_mut()[idx as usize] = val;
+        Ok(())
+    }
+
+    fn push_value(&mut self, val: Value) -> Result<()> {
+        self.runtime.stack.push(val.into());
+        Ok(())
+    }
+    
+    fn push<T : Into<Value>>(&mut self, val: T) -> Result<()> {
+        self.push_value(val.into())
+    }
+
+    fn pop_value(&mut self) -> Result<Value> {
+        self.runtime.stack.pop_value()
+    }
+
+    fn pop<T : TryFrom<Value, Error=Error>>(&mut self) -> Result<T> {
+        self.pop_value()?.try_into()
+    }
+}
+
+impl<'l> InvocationContext<'l> {
+
 
     fn current_frame(&self) -> Result<Rc<Frame>> {
         match &self.runtime.current_frame {
@@ -38,32 +85,31 @@ impl<'l> InvocationContext<'l> {
                 GlobalGet => {
                     let idx = self.next_u32()?;
                     // TODO - actual find a global
-                    self.runtime.stack.push(idx.into());
+                    self.push(idx)?;
                 },
                 LocalGet => {
                     let idx = self.next_u32()?;
-                    let val = self.current_frame()?.locals.borrow()[idx as usize];
-                    println!("GOT LOCAL {:?}", val);
-                    self.runtime.stack.push(val.into());
+                    let val = self.get_local(idx)?;
+                    self.push_value(val)?;
                 },
                 LocalSet => {
                     let idx = self.next_u32()?;
-                    let val = self.runtime.stack.pop_value()?;
-                    self.current_frame()?.locals.borrow_mut()[idx as usize] = val;
+                    let val = self.pop_value()?;
+                    self.set_local(idx, val)?;
                 }
                 I32Const => {
                     let val = self.next_u32()?;
-                    self.runtime.stack.push(val.into());
+                    self.push_value(val.into())?;
                 },
                 I32Add => {
-                    let a = self.runtime.stack.pop_i32()?;
-                    let b = self.runtime.stack.pop_i32()?;
-                    self.runtime.stack.push((a+b).into());
+                    let a = self.pop::<u32>()?;
+                    let b = self.pop::<u32>()?;
+                    self.push(a+b)?;
                 },
                 I32Sub => {
-                    let a = self.runtime.stack.pop_i32()?;
-                    let b = self.runtime.stack.pop_i32()?;
-                    self.runtime.stack.push((a-b).into());
+                    let a = self.pop::<u32>()?;
+                    let b = self.pop::<u32>()?;
+                    self.push(a-b)?;
                 },
                 I32Load => {
                     let _align = self.next_u32()?;
