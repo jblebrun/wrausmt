3748e7687427e5388db8eaca11ac9137f4b4dbbf
Jason LeBrun <jblebrun@gmail.com>
2021-04-11 18:16:13 -0700

    Divide the runtime into three separately typed stacks.
    The spec describes them as a single unified stack for convenience in the
    doc writing, but representing them as three individually type stacks
    leads to a much cleaner implementation.

--- src/runtime/instance/data_instance.rs
+++ src/runtime/instance/data_instance.rs
@@ -1,6 +1,7 @@
 /// An data instance is the runtime representation of a data segment. [Spec][Spec]
 ///
 /// It holds a vector of bytes.
+///
 /// [Spec]: https://webassembly.github.io/spec/core/exec/runtime.html#data-instances
 #[allow(dead_code)]
 pub struct DataInstance {
