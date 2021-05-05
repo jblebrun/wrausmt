353a14316f290c688eb9146f0c9493ba83d29ccf
jblebrun <jblebrun@gmail.com>
2021-05-04 19:29:58 -0700

    Refactor binary parser to use the common syntax structures (#95) (#96)
    Use the syntax module as the common format for both binary and text
    formats, remove the old module pseudo-syntax package.

--- tests/parse_and_run.rs
+++ tests/parse_and_run.rs
@@ -50,3 +50,50 @@ fn blockbr() -> Result<()> {
     assert_eq!(v1, &113.into());
     Ok(())
 }
+
+#[test]
+fn ifs() -> Result<()> {
+    let mut runtime = Runtime::new();
+    let mod_inst = runtime.load_wast("testdata/if.wat")?;
+
+    let res1 = runtime.call(&mod_inst, "simpleif", &[0.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x43.into());
+
+    let res1 = runtime.call(&mod_inst, "simpleif", &[1.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x42.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedif", &[0.into(), 0.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x143.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedif", &[0.into(), 1.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x142.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedif", &[1.into(), 0.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x43.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedif", &[1.into(), 1.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x42.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedblockif", &[0.into(), 0.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x143.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedblockif", &[0.into(), 1.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x142.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedblockif", &[1.into(), 0.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x43.into());
+
+    let res1 = runtime.call(&mod_inst, "nestedblockif", &[1.into(), 1.into()])?;
+    let v1: &Value = res1.first().unwrap();
+    assert_eq!(v1, &0x42.into());
+    Ok(())
+}
