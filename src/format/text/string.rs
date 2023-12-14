4e3e414a08f7eb19dbaa8f948a531a64582b59dc
Jason LeBrun <jblebrun@gmail.com>
2023-12-13 18:35:01 -0800

    Update spec tests and fix issues
    * Line comments should terminate with linefeed as well
    * Elem variants are LEB128 encoded, not just a byte
    
    While debugging the comment issue, I made a slight improvement to the
    string parsing code to simplify it.

--- src/format/text/string.rs
+++ src/format/text/string.rs
@@ -1,4 +1,3 @@
-use super::lex::error::{LexError, Result as LexResult};
 use std::string::FromUtf8Error;
 
 /// A WebAssembly text format string.
@@ -8,56 +7,11 @@ pub struct WasmString {
     bytes: Box<[u8]>,
 }
 
-fn parse_hex_digit(c: u8) -> Option<u8> {
-    match c {
-        b'0'..=b'9' => Some(c - b'0'),
-        b'a'..=b'f' => Some(c - b'a' + 10),
-        b'A'..=b'F' => Some(c - b'A' + 10),
-        _ => None,
-    }
-}
 impl WasmString {
-    pub fn from_bytes(bytes: &[u8]) -> LexResult<WasmString> {
-        let mut result = Vec::with_capacity(bytes.len());
-        let mut i = 0;
-        while i < bytes.len() {
-            let b = bytes[i];
-            match b {
-                b'\\' => {
-                    i += 1;
-                    let next = bytes[i];
-                    let c = if let Some(d1) = parse_hex_digit(next) {
-                        i += 1;
-                        let next = bytes[i];
-                        if let Some(d2) = parse_hex_digit(next) {
-                            (d1 << 4) | d2
-                        } else {
-                            return Err(LexError::InvalidEscape(next));
-                        }
-                    } else {
-                        match next {
-                            b't' => b'\t',
-                            b'n' => b'\n',
-                            b'r' => b'\r',
-                            b'"' => b'\"',
-                            b'\'' => b'\'',
-                            b'\\' => b'\\',
-                            b'u' => {
-                                // TODO - unicode
-                                b'0'
-                            }
-                            _ => return Err(LexError::InvalidEscape(next)),
-                        }
-                    };
-                    result.push(c);
-                }
-                b => result.push(b),
-            }
-            i += 1;
+    pub fn from_bytes(bytes: Vec<u8>) -> WasmString {
+        WasmString {
+            bytes: bytes.into(),
         }
-        Ok(WasmString {
-            bytes: result.into_boxed_slice(),
-        })
     }
 
     pub fn into_string(self) -> Result<String, FromUtf8Error> {
