a771e514c0cf60f4927538aef1fb4ac5aeab8947
jblebrun <jblebrun@gmail.com>
2024-01-06 00:07:36 -0800

    Move marker helper macro to common (#243)

---
+++ wrausmt-common/src/marker.rs
@@ -0,0 +1,28 @@
+/// A macro to create a new marker type for an existing trait.
+///
+/// ```
+/// use wrausmt_common::marker;
+/// trait MarkerTrait {}
+/// marker!(SubType: MarkerTrait);
+/// ```
+///
+/// Will generate:
+///
+/// ```
+/// trait MarkerTrait {}
+/// #[derive(Clone, Debug, Default, PartialEq)]
+/// pub struct SubType {}
+/// impl MarkerTrait for SubType {}
+/// ````
+#[macro_export]
+macro_rules! marker {
+    (
+        $(#[$($attrss:tt)*])*
+        $n:ident: $t:ty
+    ) => {
+        $(#[$($attrss)*])*
+        #[derive(Clone, Debug, Default, PartialEq)]
+        pub struct $n {}
+        impl $t for $n {}
+    };
+}
